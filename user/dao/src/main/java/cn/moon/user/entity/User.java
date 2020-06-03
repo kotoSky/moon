0af5dfa98a685a7741c7a33a4f546829fe5e77fb
koto <kotosky@163.com>
2020-06-03 18:40:57 +0800

    引入MybatisPlus

--- user/dao/src/main/java/cn/moon/user/entity/User.java
+++ user/dao/src/main/java/cn/moon/user/entity/User.java
@@ -1,10 +1,25 @@
 package cn.moon.user.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
+@TableName("t_user")
 public class User {
+
+    @NotNull(message = "ID不能为空")
+    @TableId(value = "id", type = IdType.AUTO)
     int id;
+
+    @TableField(value = "userName")
     String username;
+
+    @TableField(value = "password")
     String password;
+
 }
