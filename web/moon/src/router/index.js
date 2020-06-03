3ba018a5dfad864616093933648e8d7c6e958a9c
koto <kotosky@163.com>
2020-06-03 16:58:34 +0800

    初始化vue

---
+++ web/moon/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// 导入刚才编写的组件
+import AppIndex from '@/components/AppIndex'
+import Login from '@/components/Login'
+
+Vue.use(VueRouter);
+
+const routes = [
+  // 下面都是固定的写法
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/index',
+    name: 'AppIndex',
+    component: AppIndex
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
