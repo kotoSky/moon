e3c59157c1c7bdf4f3e94caf8f8e5d169c86cd2a
hjh <kotosky@163.com>
2020-11-24 14:53:36 +0800

    前后端配置文件更改

--- web/moon/vue.config.js
+++ web/moon/vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     productionSourceMap: true, // 不需要生产环境的设置false可以减小dist文件大小，加速构建
 
     devServer:{
-        open: true,  // npm run serve后自动打开页面
+        open: false,  // npm run serve后自动打开页面
         host: '0.0.0.0',  // 匹配本机IP地址(默认是0.0.0.0)
         port: 9090, // 开发服务器运行端口号
 
