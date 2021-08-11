f50b5395705891e549f1b1408affe2ba1950a981
hjh <kotosky@163.com>
2021-08-11 14:17:47 +0800

    自定义vite配置文件

--- web/admin-ui/vite.config.ts
+++ web/admin-ui/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    resolve: {
+        /*alias: {}*/
+    },
+    base: './', // 设置打包路径
+    server: { // 设置服务器
+        port: 3000, // 端口
+        open: false, // 浏览器是否打开
+        cors: true, // 是否跨域
+
+        // 设置代理，根据我们项目实际情况配置
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace('/api/', '/')
+            }
+        }
+    }
 })
