72e68d045878a72db31123cd34e8ec11235c796e
hjh <kotosky@163.com>
2021-08-12 13:35:29 +0800

    代码格式化配置EditorConfig、引入ESLint代码检查并初始化配置

--- web/admin-ui/src/views/axios.vue
+++ web/admin-ui/src/views/axios.vue
@@ -7,12 +7,12 @@
 import axios from '../utils/axios'
 
 axios.get("111")
-    .then((res) => {
-      console.log("请求成功")
-    })
-    .catch((err) => {
-      console.log('请求失败')
-    })
+  .then((res) => {
+    console.log("请求成功")
+  })
+  .catch((err) => {
+    console.log('请求失败')
+  })
 
 </script>
 
